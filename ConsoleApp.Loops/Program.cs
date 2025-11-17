d7b2ab1be78ef5e93ebe825bf0efd9b03ee74543
Swati <sgoyal@hyatt.com>
2025-11-17 14:19:05 +0530

    Added Exception Handline

---
+++ ConsoleApp.Loops/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Simple loop program to print Hello World 10 times
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"Hello, World!- {i}");
+//}
+//Ask the user how many times to print Hello world, and print it that many times.
+Console.Write("How many times you want to print Hello World?:  ");
+int count = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+//for (int i = 0; i < count; i++)
+//{
+//    Console.WriteLine($"Hello, World!- {i + 1}");
+//}
+while(i < count)
+{
+    Console.WriteLine($"Hello, World!- {i + 1}");
+    i++;
+}
+
--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -1,51 +1,69 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to the sample calculator");
-
-//Show calculator menu
-Console.WriteLine("Select operation:");
-Console.WriteLine("1. Addition");
-Console.WriteLine("2. Subtraction");
-Console.WriteLine("3. Multiplication");
-Console.WriteLine("4. Division");
-Console.Write("Enter choice (1-4): ");
-int choice = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-Console.Write("Enter first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Enter second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-
-int result = 0;
-switch (choice)
+int choice = 0;
+int num1, num2, result = 0;
+while (choice != -1)
 {
-    case 1:
-        result = num1 + num2;
-        break;
-    case 2:
-        result = num1 - num2;
-        break;
-    case 3:
-        result = num1 * num2;
-        break;
-    case 4:
-        if (num2 != 0)
+    try
+    {
+        Console.Clear();
+        Console.WriteLine("Welcome to the sample calculator");
+        //Show calculator menu
+        Console.WriteLine("Select operation:");
+        Console.WriteLine("1. Addition");
+        Console.WriteLine("2. Subtraction");
+        Console.WriteLine("3. Multiplication");
+        Console.WriteLine("4. Division");
+        Console.Write("Enter choice (1-4 & -1 to exit): ");
+        choice = Convert.ToInt32(Console.ReadLine());
+        if (choice == -1)
         {
-            result = num1 / num2;
+            break;
         }
-        else
+        Console.WriteLine();
+        Console.Write("Enter first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        result = 0;
+
+        switch (choice)
         {
-            Console.WriteLine("Error: Division by zero");
-            return;
+            case 1:
+                result = num1 + num2;
+                break;
+            case 2:
+                result = num1 - num2;
+                break;
+            case 3:
+                result = num1 * num2;
+                break;
+            case 4:
+               
+                result = num1 / num2;
+                break;
+            default:
+                {
+                    throw new Exception("Invalid operation choice");
+                }
         }
-        break;
-    default:
-        {
-            Console.WriteLine("Invalid choice");
-            return;
-        }
+        //Print Output
+        Console.WriteLine("Result: " + result);
+    }
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("Error: Division by zero");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    } 
+    finally
+    {
+        Console.WriteLine("Press Enter to Continue ");
+        Console.ReadLine();
+    }
 }
-Console.WriteLine("Result: " + result);
+
 Console.WriteLine("End of program");
 
 
